90b258f00c8ef0343d1485ee653f8cf894c18c6c
Peter Pfeufer <p.pfeufer@thamm-it.de>
2017-08-04 15:13:04 +0200

    Made the buttons look the same

--- templates/fitting-details/utilities/fitting-copy-permalink-to-clipboard.php
+++ templates/fitting-details/utilities/fitting-copy-permalink-to-clipboard.php
@@ -1,7 +1,7 @@
 <div class="fitting-copy-to-clipboard copy-permalink-to-clipboard col-xl-4">
 	<ul class="nav nav-pills nav-stacked">
 		<li role="presentation">
-			<span class="btn btn-default btn-copy-permalink-to-clipboard" data-clipboard-action="copy" data-clipboard-text="<?php echo \get_the_permalink(); ?>"><?php echo \__('Copy Permalink', 'eve-online-fitting-manager') ; ?></span>
+			<span type="button" class="btn btn-default btn-copy-permalink-to-clipboard" data-clipboard-action="copy" data-clipboard-text="<?php echo \get_the_permalink(); ?>"><?php echo \__('Copy Permalink', 'eve-online-fitting-manager') ; ?></span>
 		</li>
 	</ul>
 </div>
