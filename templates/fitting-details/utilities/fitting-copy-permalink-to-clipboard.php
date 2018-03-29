7c0e22045a5ddee395065bbec24fd2f1095a92a5
H.-Peter Pfeufer <info@ppfeufer.de>
2018-03-29 19:42:19 +0200

    Buttons automatically arranged in fitting view Signed-off-by:H.-Peter Pfeufer <info@ppfeufer.de>

--- templates/fitting-details/utilities/fitting-copy-permalink-to-clipboard.php
+++ templates/fitting-details/utilities/fitting-copy-permalink-to-clipboard.php
@@ -1,11 +1,4 @@
-<?php
-$col = ' col-xl-4';
-if($isUpwellStructure === true) {
-	$col = ' col-xl-6';
-} // END if($isUpwellStructure === true)
-?>
-
-<div class="fitting-copy-to-clipboard copy-permalink-to-clipboard<?php echo $col; ?>">
+<div class="fitting-copy-to-clipboard copy-permalink-to-clipboard<?php echo ' col-xl-' . $columnsPerButton; ?>">
 	<ul class="nav nav-pills nav-stacked">
 		<li role="presentation">
 			<span type="button" class="btn btn-default btn-copy-permalink-to-clipboard" data-clipboard-action="copy" data-clipboard-text="<?php echo \get_the_permalink(); ?>"><?php echo \__('Copy Permalink', 'eve-online-fitting-manager') ; ?></span>
--- templates/fitting-details/utilities/fitting-service-buttons.php
+++ templates/fitting-details/utilities/fitting-service-buttons.php
@@ -3,12 +3,41 @@
 	$isUpwellStructure = \WordPress\Plugin\EveOnlineFittingManager\Libs\Helper\FittingHelper::isUpwellStructure($shipID);
 
 	/**
+	 * Get cols count
+	 */
+	$colsCount = null;
+
+	// Check for o.smium button
+	if(isset($pluginSettings['template-detail-parts-settings']['show-osmium-link']) && $pluginSettings['template-detail-parts-settings']['show-osmium-link'] === 'yes') {
+		if($isUpwellStructure === false) {
+			$colsCount++;
+		}
+	}
+
+	// Check for copy EFT button
+	if(isset($pluginSettings['template-detail-parts-settings']['show-copy-eft']) && $pluginSettings['template-detail-parts-settings']['show-copy-eft'] === 'yes') {
+		$colsCount++;
+	}
+
+	// Check for copy link button
+	if(isset($pluginSettings['template-detail-parts-settings']['show-copy-permalink']) && $pluginSettings['template-detail-parts-settings']['show-copy-permalink'] === 'yes') {
+		$colsCount++;
+	}
+
+	$columnsPerButton = 12;
+
+	if(!is_null($columnsPerButton)) {
+		$columnsPerButton = $columnsPerButton / $colsCount;
+	}
+
+	/**
 	 * Show the Osmium fitting link
 	 */
 	if(isset($pluginSettings['template-detail-parts-settings']['show-osmium-link']) && $pluginSettings['template-detail-parts-settings']['show-osmium-link'] === 'yes') {
 		if($isUpwellStructure === false) {
 			\WordPress\Plugin\EveOnlineFittingManager\Libs\Helper\TemplateHelper::getTemplate('fitting-details/utilities/fitting-osmium', [
-				'fittingDna' => $fittingDna
+				'fittingDna' => $fittingDna,
+				'columnsPerButton' => $columnsPerButton
 			]);
 		} // END if(\WordPress\Plugin\EveOnlineFittingManager\Libs\Helper\FittingHelper::isUpwellStructure($shipID) === false)
 	} // END if(isset($pluginSettings['template-detail-parts-settings']['show-osmium-link']) && $pluginSettings['template-detail-parts-settings']['show-osmium-link'] === 'yes')
@@ -19,7 +48,8 @@
 	if(isset($pluginSettings['template-detail-parts-settings']['show-copy-eft']) && $pluginSettings['template-detail-parts-settings']['show-copy-eft'] === 'yes') {
 		\WordPress\Plugin\EveOnlineFittingManager\Libs\Helper\TemplateHelper::getTemplate('fitting-details/utilities/fitting-copy-eft-to-clipboard', [
 			'eftFitting' => $eftFitting,
-			'isUpwellStructure' => $isUpwellStructure
+			'isUpwellStructure' => $isUpwellStructure,
+			'columnsPerButton' => $columnsPerButton
 		]);
 	} // END if(isset($pluginSettings['template-detail-parts-settings']['show-copy-eft']) && $pluginSettings['template-detail-parts-settings']['show-copy-eft'] === 'yes')
 
@@ -28,7 +58,8 @@
 	 */
 	if(isset($pluginSettings['template-detail-parts-settings']['show-copy-permalink']) && $pluginSettings['template-detail-parts-settings']['show-copy-permalink'] === 'yes') {
 		\WordPress\Plugin\EveOnlineFittingManager\Libs\Helper\TemplateHelper::getTemplate('fitting-details/utilities/fitting-copy-permalink-to-clipboard', [
-			'isUpwellStructure' => $isUpwellStructure
+			'isUpwellStructure' => $isUpwellStructure,
+			'columnsPerButton' => $columnsPerButton
 		]);
 	} // END if(isset($pluginSettings['template-detail-parts-settings']['show-copy-permalink']) && $pluginSettings['template-detail-parts-settings']['show-copy-permalink'] === 'yes')
 	?>
