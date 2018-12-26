bbc4012cf60f3ecb96afd10cd1fcea1ba82491d3
Peter Pfeufer <ppfeufer@gmail.com>
2018-12-26 19:28:32 +0100

    Fixed minimum ESI client version

--- Libs/Helper/UpdateHelper.php
+++ Libs/Helper/UpdateHelper.php
@@ -46,7 +46,7 @@ class UpdateHelper extends AbstractSingleton {
      *
      * @var string
      */
-    protected $esiClientVersion = 20181202;
+    protected $esiClientVersion = 20181226;
 
     /**
      * WordPress Database Instance
