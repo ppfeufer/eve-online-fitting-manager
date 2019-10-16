484603e1d06d2a5d9840e3500e546284f398ee3e
Peter Pfeufer <info@ppfeufer.de>
2019-10-16 23:13:16 +0200

    JavaScript added as well ...

---
+++ js/radiotaxonomie.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var taxonomy = 'fitting-fleet-roles';
+
+    $('#' + taxonomy + 'checklist li :radio, #' + taxonomy + 'checklist-pop :radio').on('click', function () {
+        var t = $(this), c = t.is(':checked'), id = t.val();
+
+        $('#' + taxonomy + 'checklist li :radio, #' + taxonomy + 'checklist-pop :radio').prop('checked', false);
+        $('#in-' + taxonomy + '-' + id + ', #in-popular-' + taxonomy + '-' + id).prop('checked', c);
+    });
+});
